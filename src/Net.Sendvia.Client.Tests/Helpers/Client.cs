1ecfaec9ed88d0ac3d367337f4af4ad73ad04feb
Alxwest <alxwest@gmail.com>
2015-03-03 18:02:30 +0000

    Added Carrier Create test.

---
+++ src/Net.Sendvia.Client.Tests/Helpers/Client.cs
@@ -0,0 +1,17 @@
+﻿using Net.Sendvia.Tests.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Sendvia.Helpers
+{
+    public static class Client 
+    {
+        public static Net.Sendvia.Client Create()
+        {
+            return new Sendvia.Client(Settings.Default.Client_Id, Settings.Default.Secret);
+        }
+    }
+}
