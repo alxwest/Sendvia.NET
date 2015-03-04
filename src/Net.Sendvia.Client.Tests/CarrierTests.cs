f47d42389f194a7524e5ab592cda268d0054bdfa
Alxwest <alxwest@gmail.com>
2015-03-04 08:53:52 +0000

    Added CarrierTests

---
+++ src/Net.Sendvia.Client.Tests/CarrierTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Net.Sendvia.Tests
+{
+    [TestClass]
+    public class CarrierTests
+    {
+        [TestMethod]
+        public void CreateCarrier()
+        {
+            var client = Helpers.Client.Create();
+
+            var carrier = new Models.Carrier
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Carrier",
+                Editable = false,
+                Private = true
+            };
+
+            var r = client.Carrier_Create(carrier);
+
+        }
+    }
+}
